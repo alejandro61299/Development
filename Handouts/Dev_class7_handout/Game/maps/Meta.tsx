11a6535ec040e08b9885d6e757151e509ea8395b
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2018-10-10 12:06:31 +0200

    Added handout 6

---
+++ Handouts/Dev_class7_handout/Game/maps/Meta.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Meta" tilewidth="64" tileheight="32" tilecount="4" columns="2">
+ <grid orientation="orthogonal" width="64" height="64"/>
+ <image source="meta.png" width="128" height="64"/>
+</tileset>
